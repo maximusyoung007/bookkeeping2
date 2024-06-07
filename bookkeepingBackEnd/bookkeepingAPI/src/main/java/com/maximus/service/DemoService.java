73f4c7b6d25180c2891a0a9c4d80933d3758ee12
maximus <1091261998@qq.com>
2024-06-07 11:33:41 +0800

    dubbo demo

---
+++ bookkeepingBackEnd/bookkeepingAPI/src/main/java/com/maximus/service/DemoService.java
@@ -0,0 +1,12 @@
+package com.maximus.service;
+
+/**
+ * <pre>
+ * Modify Information:
+ * Author       Date          Description
+ * ============ ============= ============================
+ * maximus         2024/6/7      create
+ */
+public interface DemoService {
+    String sayHello(String name);
+}
---
+++ bookkeepingBackEnd/bookkeepingService/src/main/java/com/maximus/serviceImpl/DemoServiceImpl.java
@@ -0,0 +1,12 @@
+package com.maximus.serviceImpl;
+
+/**
+ * <pre>
+ * Modify Information:
+ * Author       Date          Description
+ * ============ ============= ============================
+ * maximus         2024/6/7      create
+ */
+public class DemoServiceImpl implements DemoService {
+
+}
