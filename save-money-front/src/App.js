0c78959601edd04a20ff49cb1402f99cf28fb56e
杨文俊 <1091261998@qq.com>
2024-07-07 22:56:37 +0800

    基本页面布局

--- save-money-front/src/App.js
+++ save-money-front/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    AccountBookOutlined,
+    PropertySafetyOutlined,
+    BarChartOutlined
+  } from '@ant-design/icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Breadcrumb, Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
 }
-
+const items = [
+  getItem('账本', '1', <AccountBookOutlined />),
+  getItem('资产', '2', <PropertySafetyOutlined />),
+  getItem('统计', '3', <BarChartOutlined />),
+];
+const App = () => {
+  const [collapsed, setCollapsed] = useState(true);
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider collapsed={true}
+             style={{background: "#ffffff", width: "50px"}}>
+        <div className="logo" style={{height: '64px'}} />
+        <Menu theme="light" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="site-layout-background"
+          style={{
+            padding: 0,
+            background: "#ffffff"
+          }}
+        />
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
 export default App;
