d55f9c653b3c4b8ff81f397fb2a5c3b8e629319c
杨文俊 <1091261998@qq.com>
2024-07-07 19:11:03 +0800

    zookeeper、dubbo整合

--- bookkeepingBackEnd/bookkeepingService/src/main/java/com/maximus/serviceImpl/DemoServiceImpl.java
+++ bookkeepingBackEnd/bookkeepingService/src/main/java/com/maximus/serviceImpl/DemoServiceImpl.java
@@ -13,6 +13,7 @@ import org.apache.dubbo.config.annotation.DubboService;
 @DubboService
 public class DemoServiceImpl implements DemoService {
 
+    @Override
     public String sayHello(String name) {
         return "hello" + name;
     }
