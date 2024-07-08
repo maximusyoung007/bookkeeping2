4ce631db06ce79cf7abed6d275687d087655a59b
杨文俊 <1091261998@qq.com>
2024-07-09 00:21:19 +0800

    路由

--- save-money-front/src/App.js
+++ save-money-front/src/App.js
@@ -4,9 +4,13 @@ import {
     BarChartOutlined
   } from '@ant-design/icons';
 
+import Account from "./account/account";
+import AddBooking from "./booking/AddBooking";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { Breadcrumb, Layout, Menu } from 'antd';
 import React, { useState } from 'react';
 const { Header, Content, Footer, Sider } = Layout;
+
 function getItem(label, key, icon, children) {
   return {
     key,
@@ -16,12 +20,20 @@ function getItem(label, key, icon, children) {
   };
 }
 const items = [
-  getItem('账本', '1', <AccountBookOutlined />),
-  getItem('资产', '2', <PropertySafetyOutlined />),
-  getItem('统计', '3', <BarChartOutlined />),
+  getItem('账本', '/Account', <AccountBookOutlined />),
+  getItem('资产', '/AddBooking',  <PropertySafetyOutlined />),
+  getItem('统计', '/Cal', <BarChartOutlined />),
 ];
 const App = () => {
   const [collapsed, setCollapsed] = useState(true);
+
+  const navigate = useNavigate();
+
+  const onClick = (e) => {
+      console.log(e)
+      navigate(e.key, {replace : true})
+  }
+
   return (
     <Layout
       style={{
@@ -31,39 +43,14 @@ const App = () => {
       <Sider collapsed={true}
              style={{background: "#ffffff", width: "50px"}}>
         <div className="logo" style={{height: '64px'}} />
-        <Menu theme="light" defaultSelectedKeys={['1']} mode="inline" items={items} />
+        <Menu theme="light" defaultSelectedKeys={['1']} mode="inline" items={items} onClick={onClick}/>
       </Sider>
       <Layout className="site-layout">
-        <Header
-          className="site-layout-background"
-          style={{
-            padding: 0,
-            background: "#ffffff"
-          }}
-        />
-        <Content
-          style={{
-            margin: '0 16px',
-          }}
-        >
-          <Breadcrumb
-            style={{
-              margin: '16px 0',
-            }}
-          >
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-          <div
-            className="site-layout-background"
-            style={{
-              padding: 24,
-              minHeight: 360,
-            }}
-          >
-            Bill is a cat.
-          </div>
-        </Content>
+          <Routes>
+              <Route exact path="/Account" element={<Account />}/>
+              <Route exact path="/AddBooking" element={<AddBooking />}/>
+              <Route exact path="/" element={<Account/>}/>
+          </Routes>
       </Layout>
     </Layout>
   );
---
+++ save-money-front/src/account/account.js
@@ -0,0 +1,19 @@
+import {
+    Button,
+    Cascader,
+    DatePicker,
+    Form,
+    Input,
+    InputNumber,
+    Radio,
+    Select,
+    Switch,
+    TreeSelect,
+} from 'antd';
+import React, { useState } from 'react';
+const Accounting = () => {
+  return (
+    <div>Accounting</div>
+  );
+};
+export default Accounting;
